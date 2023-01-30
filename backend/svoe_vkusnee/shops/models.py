1b81f8a73ca94114ad4d735b7343d39968213093
Денис Малых <malykhdenis@gmail.com>
2023-01-31 00:53:17 +0300

    models except User and Shop

--- backend/svoe_vkusnee/shops/models.py
+++ backend/svoe_vkusnee/shops/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Shop(models.Model):
+    """Производители."""
+    pass
+
+
+class Category(models.Model):
+    """Категории."""
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    photo = models.ImageField()
+
+
+class Subcategory(models.Model):
+    """Подкатегории."""
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+
+
+class Product(models.Model):
+    """Продукты."""
+    name = models.CharField(max_length=100)
+    photo = models.ImageField()
+    description = models.TextField()
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.SET_NULL)
+
+
+class FavoriteShop(models.Model):
+    """Магазин в избранном."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+
+
+class FavoriteProduct(models.Model):
+    """Продукт в избранном."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
