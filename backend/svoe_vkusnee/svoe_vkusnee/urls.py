79b9b05ee6316476025cbd17e67f7b2a36a91d1b
malykhdenis <malykhdenis@gmail.com>
2023-04-04 19:37:16 +0300

    runserver_page

--- backend/svoe_vkusnee/svoe_vkusnee/urls.py
+++ backend/svoe_vkusnee/svoe_vkusnee/urls.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
+
+def runserver_page(request):
+    return HttpResponse(
+        "<ul><li><a href='admin/'>admin</a></li>"
+        "<li><a href='api/'>api</a></li></ul>",
+        content_type="text/html",
+        charset="utf-8"
+    )
+
+
 urlpatterns = [
+    path('', runserver_page),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
